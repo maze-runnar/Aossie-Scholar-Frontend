82ce1a15789409f07a354c002b484ed9072db8ee
sundaram <dubeysundaram057@gmail.com>
2020-03-25 09:31:51 +0530

    Updates

---
+++ precache-manifest.39afff0912bb1f9c5106fba612a7bbbc.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a785c6b0d2f610a2d85a276239a9d9d3",
+    "url": "/Aossie-Scholar-Frontend/index.html"
+  },
+  {
+    "revision": "185f9f2c51356b413b19",
+    "url": "/Aossie-Scholar-Frontend/static/css/2.9ddb28ea.chunk.css"
+  },
+  {
+    "revision": "5213bfa3e4bd4d34f55f",
+    "url": "/Aossie-Scholar-Frontend/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "185f9f2c51356b413b19",
+    "url": "/Aossie-Scholar-Frontend/static/js/2.6bfa6d3d.chunk.js"
+  },
+  {
+    "revision": "be77fb0928e0ec696d6de489811cf826",
+    "url": "/Aossie-Scholar-Frontend/static/js/2.6bfa6d3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5213bfa3e4bd4d34f55f",
+    "url": "/Aossie-Scholar-Frontend/static/js/main.8bc53688.chunk.js"
+  },
+  {
+    "revision": "2af4b54165b12bcb4191",
+    "url": "/Aossie-Scholar-Frontend/static/js/runtime-main.364c12a0.js"
+  },
+  {
+    "revision": "1a6f8e555a717c5734e5edcf60b79ffc",
+    "url": "/Aossie-Scholar-Frontend/static/media/1.1a6f8e55.jpg"
+  },
+  {
+    "revision": "f93ca61c7f21c00dc135ee68c033e85a",
+    "url": "/Aossie-Scholar-Frontend/static/media/AOSSIE.f93ca61c.png"
+  },
+  {
+    "revision": "7da5dfa5db0a18165d3f8fad0010e37b",
+    "url": "/Aossie-Scholar-Frontend/static/media/TNC.7da5dfa5.jpg"
+  },
+  {
+    "revision": "5c47038f2f4e27221f532345bdffbbc8",
+    "url": "/Aossie-Scholar-Frontend/static/media/gindex.5c47038f.png"
+  },
+  {
+    "revision": "bbd05b15c304e7d532f1f4d758c80fd0",
+    "url": "/Aossie-Scholar-Frontend/static/media/gitlab.bbd05b15.png"
+  },
+  {
+    "revision": "ae27d7ff9d7f21d4d0368a9e9ac015cf",
+    "url": "/Aossie-Scholar-Frontend/static/media/gitter.ae27d7ff.jpg"
+  },
+  {
+    "revision": "716b7b7d9443cc4f2947a0512fd192b5",
+    "url": "/Aossie-Scholar-Frontend/static/media/hindex.716b7b7d.png"
+  },
+  {
+    "revision": "b6487141a9386d356606ff1d3dcb0d0d",
+    "url": "/Aossie-Scholar-Frontend/static/media/hindexvisual.b6487141.png"
+  },
+  {
+    "revision": "f92b06ed24684117aaa417c49690e176",
+    "url": "/Aossie-Scholar-Frontend/static/media/mindex.f92b06ed.png"
+  },
+  {
+    "revision": "d92624fd1097f753c16541298e6eadc4",
+    "url": "/Aossie-Scholar-Frontend/static/media/oindex.d92624fd.png"
+  },
+  {
+    "revision": "e5cc4acf545fc090ad58bd144af1da27",
+    "url": "/Aossie-Scholar-Frontend/static/media/search.e5cc4acf.jpg"
+  }
+]);
